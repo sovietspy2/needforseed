bb273d6cb5c3b83d0befa2c935d25c37c5c1fee3
SovietSpy2 <sovietspy2@yandex.com>
2019-02-13 12:38:08 +0100

    fixed problem in local dev enviorment

--- src/api.js
+++ src/api.js
@@ -5,17 +5,17 @@ const API_URL = process.env.REACT_APP_API_URL || ''; // for cors
 
 const api = {
     // Register login auth etc
-    VALIDATE_USERNAME: API_URL+'/api/checkUsername',
-    REGISTER: API_URL+'/api/register',
-    LOGIN: API_URL+'/api/authenticate',
-    CHECK_TOKEN: API_URL+'/api/checkToken',
-    LOGOUT: API_URL+'/api/logout',
+    VALIDATE_USERNAME: API_URL+'/checkUsername',
+    REGISTER: API_URL+'/register',
+    LOGIN: API_URL+'/authenticate',
+    CHECK_TOKEN: API_URL+'/checkToken',
+    LOGOUT: API_URL+'/logout',
 
     //posts
-    NEXT_POST: API_URL+'/api/nextPost',
-    PREVIOUS_POST: API_URL+'/api/previousPost',
-    LATEST_POST: API_URL+'/api/lastPost',
-    SAVE_POST: API_URL+'/api/savePost'
+    NEXT_POST: API_URL+'/nextPost',
+    PREVIOUS_POST: API_URL+'/previousPost',
+    LATEST_POST: API_URL+'/lastPost',
+    SAVE_POST: API_URL+'/savePost'
 
 
 }
