817c5967b0d6fc7caaafa3ba6fd79a4d5b740103
SovietSpy2 <sovietspy2@yandex.com>
2019-02-13 08:30:08 +0100

    merge

--- src/containers/loginPage/index.js
+++ src/containers/loginPage/index.js
@@ -29,7 +29,6 @@ export default class Login extends PureComponent {
         console.log(this.state.username, this.state.password);
         console.log("uh oh submitted");
         axios.post('/api/authenticate', {
-          credentials: 'include',  
           username: this.state.username,
           password: this.state.password
         })
